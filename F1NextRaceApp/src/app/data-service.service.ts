bc82feae15985401ccd575c3397514120967e899
Jordan Parviainen <jordus100@gmail.com>
2022-05-04 20:09:07 +0200

    Added dev/prod config and fixed race weekend timer 1. Added a value called 'apiUrl' in environments/ files to avoid hardcoding the backend API value into code and having to change it for production/development builds. 2. Made a small fix to the timer component so that the timer values never go below zero.

--- F1NextRaceApp/src/app/data-service.service.ts
+++ F1NextRaceApp/src/app/data-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
 import {RaceWeekend} from "./race-weekend";
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,7 @@ export class DataServiceService {
 
   private apiUrl: string;
   constructor(private http: HttpClient) {
-    this.apiUrl = "http://localhost:8080/raceweekend";
-    //this.apiUrl = "http://f1nextrace-backend.azurewebsites.net/raceweekend";
+    this.apiUrl = environment.apiUrl;
   }
   public getRaceWeekendData(): Observable<RaceWeekend> {
     return this.http.get<RaceWeekend>(this.apiUrl+'/get');
